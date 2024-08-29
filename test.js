39646cc60fce20609aff3e9fa980de882c293449
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-08-29 18:21:26 +0500

    setting , defineProperty, getOwnPropertyDescriptor

--- test.js
+++ test.js
@@ -1,16 +1,63 @@
-let obj = {
-  firstName: "Azizbek",
-  lastName: "Abduhakimov",
+// let obj = {
+//   firstName: "Azizbek",
+//   lastName: "Abduhakimov",
 
-  get fullName() {
-    return this.firstName + " " + this.lastName;
-  },
+//   get fullName() {
+//     return this.firstName + " " + this.lastName;
+//   },
 
-  set fullName(name) {
-    [this.firstName, this.lastName] = name.split(" ");
-  },
-};
+//   set fullName(name) {
+//     [this.firstName, this.lastName] = name.split(" ");
+//   },
+// };
+// obj.fullName = "Abdulaziz Programmer";
+// console.log(obj.fullName);
 
-obj.fullName = "Abdulaziz Programmer";
+// !setting
 
-console.log(obj.fullName);
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Object.isFrozen(Math));
+
+// console.log(Object.getOwnPropertyDescriptor(Math, "PI"));
+// console.log(Object.getOwnPropertyDescriptor(obj, "firstName"));
+
+// for (let key in obj) {
+//   console.log(key);
+// }
+// {
+//     value: 'Azizbek',
+//     writable: true, // o'zgartrib bo'lishi
+//     enumerable: true, for orqali aylantish
+//     configurable: true // delete qilib bo'lish bo'lmaslik
+//   }
+
+// let obj = {
+//   firstName: "Azizbek",
+//   lastName: "Abduhakimov",
+
+//   get fullName() {
+//     return this.firstName + " " + this.lastName;
+//   },
+
+//   set fullName(name) {
+//     [this.firstName, this.lastName] = name.split(" ");
+//   },
+// };
+
+let obj = {};
+
+Object.defineProperty(obj, "firstName", {
+  value: "Abdulaziz",
+  writable: true,
+  enumerable: false,
+  configurable: false,
+});
+
+obj.firstName = "Azizbek";
+delete obj.firstName;
+
+for (let key in obj) {
+  console.log(key);
+}
+console.log(obj);
