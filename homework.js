7ac9c5f40588e07338da2786ec77dfe45f53c5b7
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-08-30 17:15:16 +0500

    String constructoriga quyidagi metodlarni custom variantini qo’shing.

--- homework.js
+++ homework.js
@@ -91,3 +91,89 @@
 // };
 
 // console.log(arr.sum());
+
+// * trim → trm
+
+// String constructoriga trm metodini qo'shish
+String.prototype.trm = function () {
+  let start = 0;
+  let end = this.length - 1;
+  // Boshlanishdagi bo'sh joylarni topish
+  while (start <= end && this[start] === " ") {
+    start++;
+    console.log(start);
+  }
+  while (end >= start && this[end] === " ") {
+    end--;
+    console.log(end);
+  }
+  // Bo'sh joylarni olib tashlangan substringni qaytarish
+  console.log(start, end);
+
+  return this.substring(start, end + 1);
+};
+
+// Trm metodini sinab ko'rish
+const myString = "  Salom Dunyo!  ";
+console.log(myString.trm()); // "Salom Dunyo!"
+
+// ! toUpperCase → upperCase
+// let str = "Aziz Abduhakimov";
+
+// String.prototype.upperCase = function () {
+//   let res = "";
+
+//   for (let el of this) {
+//     let pos = el.charCodeAt();
+
+//     if (pos >= 97 && pos <= 122) {
+//       res += String.fromCharCode(pos - 32);
+//     } else {
+//       res += el;
+//     }
+//   }
+
+//   return res;
+// };
+
+// console.log(str.upperCase());
+
+//! includes → has
+// String constructoriga has metodini qo'shish
+String.prototype.has = function (substring) {
+  console.log(this.indexOf(substring));
+  console.log(substring);
+
+  return this.indexOf(substring) !== -1;
+};
+
+let str = "Azizbek Abduhakimov";
+// Sinov uchun kod
+console.log(str.has("Azizbek"));
+
+// slice → cut
+
+// let cutStr = "Azizbek Abduhakimov";
+
+// String.prototype.cut = function (a, b) {
+//   let res = "";
+//   for (let i = a; i < b; i++) {
+//     res += this[i];
+//   }
+
+//   return res;
+// };
+
+// console.log(cutStr.cut(1, 9));
+
+//! repeat → rpt
+
+let strRpt = "Aziz";
+String.prototype.rpt = function (n) {
+  let res = "";
+  for (let i = 0; i < n; i++) {
+    res += this + " ";
+  }
+  return res;
+};
+console.log(strRpt.rpt(3));
